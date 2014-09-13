1f3ec08a37a178bfc0752231c95003acada5a8c8
jiunjiun <quietmes@gmail.com>
2014-09-14 00:29:08 +0800

    update

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -3,7 +3,11 @@ class Bus < ActiveRecord::Base
 
   def self.latest_depart
     latest_depart = where("depart > ?", Time.zone.now.change(year: 2014, month: 6, day:1)).order(:depart)
-    order(:depart) if latest_depart.size <= 0
+    if latest_depart.size <= 0
+      order(:depart)
+    else
+      latest_depart
+    end
   end
 
   before_save :update_depart
