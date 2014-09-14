a312af388686180a346d75ea38289e466fdf2bfc
jiunjiun <quietmes@gmail.com>
2014-09-14 23:38:59 +0800

    修正跨日的問題

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -2,7 +2,8 @@ class Bus < ActiveRecord::Base
   enum category: [ :master, :safety, :security ]
 
   def self.latest_depart
-    where("depart > ?", Time.zone.now.change(year: 2014, month: 6, day:1)).order(:depart)
+    latest_depart = where("depart > ?", Time.zone.now.change(year: 2014, month: 6, day:1)).order(:depart)
+    order(:depart) if latest_depart.size <= 0
   end
 
   before_save :update_depart
