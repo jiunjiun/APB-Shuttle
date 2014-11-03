fba907c1de9bc9a02a84d5a4c9b37ed0b74cfccc
jiunjiun <quietmes@gmail.com>
2014-11-03 21:04:13 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :init_seesion
   protected
     def init_seesion
-      reset_session if session[:depart] <= 1.minutes.ago && request.fullpath == root_path if session[:depart]
+      reset_session if session[:depart] <= 1.minutes.ago && request.fullpath == root_path if session[:depart].nil?
       session[:now_num]   ||= BusCache.where({bus_id: Bus.recent_depart.id}).first.id.to_i
     end
 end
